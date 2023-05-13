3533b80ff84c870e34695c670575aa6d252952c2
tahseenagwani <tahseenagwawni786@gmail.com>
2023-05-13 23:19:39 +0530

    doit

--- src/component/SearchData.js
+++ src/component/SearchData.js
@@ -1,10 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 import WeatherDetails from './WeatherDetails';
 
 function SearchData() {
   const [searchTerm,setSearchTerm]=useState("mumbai");
-  console.log(searchTerm);
+  const [tempInfo,settempInfo]=useState({})
+ const getWeatherInfo=async()=>{
+
+  try{
+   let url=`http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=694eaed0982de1ea8f9ad988e99fbee7`;
+  let res=await fetch(url)
+  let data=await res.json()
+  console.log(data)
+  const{temp,pressure,humidity}=data.main;
+  const {main:weatherType}=data.weather[0]
+  const {name}=data;
+  const {speed}=data.wind
+const {country,sunset}=data.sys
+
+let myNewWeatherInfo={
+temp,
+humidity,
+pressure,
+weatherType,
+name,
+speed,
+country,
+sunset
+
+}
+settempInfo(myNewWeatherInfo);
+  console.log(weatherType);
+  }
+  catch(error){
+    console.log(error)
+  }
+
+}
+//`http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}}&appid=694eaed0982de1ea8f9ad988e99fbee7`
+useEffect(()=>{
+getWeatherInfo()
+
+
+},[]) 
+ 
   return (
     <>
     <div className="wrap">
@@ -16,12 +55,12 @@ function SearchData() {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className="searchButton" >
+        <button className="searchButton" onClick={getWeatherInfo}>
           Search
         </button>
       </div>
     </div>
-    <WeatherDetails/>
+    <WeatherDetails newdata={tempInfo}/>
     </>
   )
 }
--- src/component/WeatherDetails.js
+++ src/component/WeatherDetails.js
@@ -1,21 +1,112 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function WeatherDetails() {
+function WeatherDetails(prop) {
+    console.log(prop)
+const [weatherState,setWeatherState]=useState("")
+
+    let date =new Date(prop.newdata.sunset * 1000);
+    let timestr=`${date.getHours()} : ${date.getMinutes()}`
+
+   
+
+    useEffect(()=>{
+        if(prop.newdata.weatherType){
+            switch(prop.newdata.weatherType){
+                case "Clouds":
+          setWeatherState("wi-day-cloudy");
+          break;
+        case "Haze":
+          setWeatherState("wi-fog");
+          break;
+        case "Clear":
+          setWeatherState("wi-day-sunny");
+          break;
+        case "Mist":
+          setWeatherState("wi-dust");
+          break;
+        case "Rain":
+          setWeatherState("wi-day-rain");
+          break;
+
+        default:
+          setWeatherState("wi-day-sunny");
+          break;
+
+            }
+
+
+        }
+
+    },[prop.newdata.weatherType])
   return (
     <>
     <article className='widget'>
         <div className="weatherIcon">
-        <i className="wi wi-day-sunny"></i>
+        <i className={`wi ${weatherState}`}></i>
         </div>
-          <div className="weatherInfo">
+    <div className="weatherInfo">
           <div className="temperature">
-            <span>22.99</span>
+            <span>{prop.newdata.temp}&deg;</span>
         </div>
     <div className="description">
-    <div className="weatherCondition">sunny</div>
-        <div className="place"> Mumbai,IN </div>
+    <div className="weatherCondition">{prop.newdata.weatherType}</div>
+        <div className="place">{prop.newdata.name},{prop.newdata.country}
+    </div>
     </div>
         </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+        <div className="extra-temp">
+            <div className="temp-info-minmax">
+                <div className="two-sided-section">
+                    <p>
+                        <i className={"wi wi-sunset"}></i>
+                    </p>
+                    <p className="extra-info-leftside">
+                    {timestr} PM
+                        <br/>
+                        Time
+                    </p>
+                </div>
+            
+                <div className="two-sided-section">
+                    <p>
+                        <i className={"wi wi-humidity"}></i>
+                    </p>
+                    <p className="extra-info-leftside">
+                    {prop.newdata.humidity}
+                      <br/>
+                        Humiddity
+                    </p>
+                </div>
+                </div>
+            <div className="temp-info-minmax">
+                <div className="two-sided-section">
+                    <p>
+                        <i className={"wi wi-rain"}></i>
+                    </p>
+                    <p className="extra-info-leftside">
+                    {prop.newdata.pressure}
+                      <br/>
+                        Pressure
+                    </p>
+                </div>
+            
+        
+                <div className="two-sided-section">
+                    <p>
+                        <i className={"wi wi-strong-wind"}></i>
+                    </p>
+                    <p className="extra-info-leftside">
+                    {prop.newdata.speed}
+                      <br/>
+                        Speed
+                    </p>
+                </div>
+            </div>
+            
+
+        </div>
+    
     </article>
     
     
